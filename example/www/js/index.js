9a214fc0b7eb4862f422aeb78247456203ec995b
yabbi-sdk-developer <sdk@yabbi.me>
2023-02-20 16:31:19 +0300

    feat: add js interface

--- example/www/js/index.js
+++ example/www/js/index.js
@@ -2,6 +2,11 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 var YabbiAds;
 
+const AdType = {
+    INTERSTITIAL: 1,
+    REWARDED: 3,
+};
+
 function onDeviceReady() {
     YabbiAds = cordova.require('cordova.plugin.yabbiads.YabbiAdsPlugin');
     _init();
@@ -9,6 +14,7 @@ function onDeviceReady() {
 
 function _init() {
     YabbiAds.initialize(EnvVariables.YABBI_PUBLISHER_ID, EnvVariables.YABBI_INTERSTITIAL_ID, EnvVariables.YABBI_REWARDED_ID);
+    YabbiAds.setUserConsent(true);
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('load_interstitial').addEventListener("click", _loadInterstitial);
@@ -18,28 +24,76 @@ function _init() {
     document.getElementById('show_rewarded').addEventListener("click", _showRewarded);
     document.getElementById('destroy_rewarded').addEventListener("click", _destroyRewarded);
 
+    _initializeInterstitialListener();
+    _initializeRewardedListener();
+
+}
+
+function _initializeInterstitialListener() {
+    window.addEventListener('onInterstitialLoaded', function (adInfo) {
+        _logEvent('onInterstitialLoaded');
+    });
+    window.addEventListener('onInterstitialLoadFailed', function (adInfo) {
+        _logEvent('onInterstitialLoadFailed');
+    });
+    window.addEventListener('onInterstitialShown', function (adInfo) {
+        _logEvent('onInterstitialShown');
+    });
+    window.addEventListener('onInterstitialShowFailed', function (adInfo) {
+        _logEvent('onInterstitialShowFailed');
+    });
+    window.addEventListener('onInterstitialClosed', function (adInfo) {
+        _logEvent('onInterstitialClosed');
+    });
+}
+
+
+function _initializeRewardedListener() {
+    window.addEventListener('onRewardedLoaded', function (adInfo) {
+        _logEvent('onRewardedLoaded');
+    });
+    window.addEventListener('onRewardedLoadFailed', function (adInfo) {
+        _logEvent('onRewardedLoadFailed');
+    });
+    window.addEventListener('onRewardedShown', function (adInfo) {
+        _logEvent('onRewardedShown');
+    });
+    window.addEventListener('onRewardedShowFailed', function (adInfo) {
+        _logEvent('onRewardedShowFailed');
+    });
+    window.addEventListener('onRewardedClosed', function (adInfo) {
+        _logEvent('onRewardedClosed');
+    });
+    window.addEventListener('onRewardedFinished', function (adInfo) {
+        _logEvent('onRewardedFinished');
+    });
 }
 
 function _loadInterstitial() {
-    console.log("_loadInterstitial");
+    YabbiAds.loadAd(AdType.INTERSTITIAL);
 }
 
 function _showInterstitial() {
-    console.log("_showInterstitial");
+    YabbiAds.showAd(AdType.INTERSTITIAL);
 }
 
 function _destroyInterstitial() {
-    console.log("_destroyInterstitial");
+    YabbiAds.destroyAd(AdType.INTERSTITIAL);
 }
 
 function _loadRewarded() {
-    console.log("_loadRewarded");
+    YabbiAds.loadAd(AdType.REWARDED);
 }
 
 function _showRewarded() {
-    console.log("_showRewarded");
+    YabbiAds.showAd(AdType.REWARDED);
 }
 
 function _destroyRewarded() {
-    console.log("_destroyRewarded");
+    YabbiAds.destroyAd(AdType.REWARDED);
+}
+
+function _logEvent(message) {
+    var logger = document.getElementById('logger');
+    logger.textContent = '${logger.innerHTML}\n ${message}'
 }
