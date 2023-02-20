07be17709988e0582baed1f4e45aeb9103b5daf3
yabbi-sdk-developer <sdk@yabbi.me>
2023-02-20 14:01:13 +0300

    feat: add demo app

--- example/www/js/index.js
+++ example/www/js/index.js
@@ -1,29 +1,41 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+    _init();
+}
 
+
+function _init() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    document.getElementById('load_interstitial').addEventListener("click", _loadInterstitial);
+    document.getElementById('show_interstitial').addEventListener("click", _showInterstitial);
+    document.getElementById('destroy_interstitial').addEventListener("click", _destroyInterstitial);
+    document.getElementById('load_rewarded').addEventListener("click", _loadRewarded);
+    document.getElementById('show_rewarded').addEventListener("click", _showRewarded);
+    document.getElementById('destroy_rewarded').addEventListener("click", _destroyRewarded);
+
+}
+
+function _loadInterstitial() {
+    console.log("_loadInterstitial");
+}
+
+function _showInterstitial() {
+    console.log("_showInterstitial");
+}
+
+function _destroyInterstitial() {
+    console.log("_destroyInterstitial");
+}
+
+function _loadRewarded() {
+    console.log("_loadRewarded");
+}
+
+function _showRewarded() {
+    console.log("_showRewarded");
+}
+
+function _destroyRewarded() {
+    console.log("_destroyRewarded");
 }
