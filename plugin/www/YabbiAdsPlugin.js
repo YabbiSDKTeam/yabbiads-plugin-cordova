81e9feac5160c3a5c0a1522c3611517eeb90c422
yabbi-sdk-developer <sdk@yabbi.me>
2023-02-21 15:00:30 +0300

    feat: update mthods

--- plugin/www/YabbiAdsPlugin.js
+++ plugin/www/YabbiAdsPlugin.js
@@ -1,30 +1,50 @@
 var exec = require('cordova/exec');
 var cordova = require('cordova');
 
-const VERSION = '1.0.0';
+const _VERSION = '1.0.0';
 
-function isFunction(functionObj) {
+function _isFunction(functionObj) {
     return typeof functionObj === 'function';
 }
 
-function callNative(name, params = [], successCallback = null, errorCallback = null) {
+function _callNative(name, params = [], successCallback = null, errorCallback = null) {
     cordova.exec(successCallback, errorCallback, 'YabbiAdsPlugin', name, params);
 }
 
 
+const _AdType = {
+    INTERSTITIAL: 1,
+    REWARDED: 3,
+};
+
+
 var YabbiAds = {
+
+    version: _VERSION,
 
     // NOTE: We have to store states in JS as workaround for callback-based API
     // since Cordova does not allow for synchronous returns
     initialized: false,
-    hasUserConsentValue: null,
-    version: VERSION,
+    hasUserConsentValue: false,
+    canLoadInterstitial: false,
+    canLoadRewarded: false,
+    isInterstitialLoaded: false,
+    isRewardedLoaded: false,
 
 
     initialize: function (publisherId, interstitialId, rewardedId) {
-        callNative('initialize', [publisherId, interstitialId, rewardedId], function (config) {
-            YabbiAds.initialized = true;
-            YabbiAds.hasUserConsentValue = config.hasUserConsent;
+        _callNative('initialize', [publisherId, interstitialId, rewardedId], function (config) {
+            this.initialized = true;
+            this.hasUserConsentValue = config.hasUserConsent;
+            if (publisherId) {
+                if (interstitialId) {
+                    this.canLoadInterstitial = true;
+                }
+
+                if (rewardedId) {
+                    this.canLoadRewarded = true;
+                }
+            }
         });
     },
 
@@ -34,7 +54,7 @@ var YabbiAds = {
 
     setUserConsent: function (hasUserConsent) {
         this.hasUserConsentValue = hasUserConsent;
-        callNative('setUserConsent', [hasUserConsent]);
+        _callNative('setUserConsent', [hasUserConsent]);
     },
 
     hasUserConsent: function () {
@@ -42,37 +62,75 @@ var YabbiAds = {
     },
 
     loadAd: function (adType) {
-        callNative('loadAd', [adType]);
+        _callNative('loadAd', [adType]);
     },
 
     showAd: function (adType) {
-        callNative('showAd', [adType]);
+        _callNative('showAd', [adType]);
     },
 
     destroyAd: function (adType) {
-        callNative('destroyAd', [adType]);
+        switch (adType) {
+            case _AdType.INTERSTITIAL:
+                this.canLoadInterstitial = true;
+                this.isInterstitialLoaded = false;
+                break;
+            case _AdType.REWARDED:
+                this.canLoadRewarded = true;
+                this.isRewardedLoaded = false;
+                break;
+        }
+
+        _callNative('destroyAd', [adType]);
     },
 
     setCustomParams(key, value) {
-        callNative('setCustomParams', [key, value]);
+        _callNative('setCustomParams', [key, value]);
     },
 
-    canLoadAd(adType, callback) {
-        callNative('canLoadAd', [adType], function (canLoad) {
-            if (isFunction(callback)) {
-                callback(canLoad);
-            }
-        });
+    canLoadAd(adType) {
+        switch (adType) {
+            case _AdType.INTERSTITIAL:
+                return this.canLoadInterstitialAd;
+            case _AdType.REWARDED:
+                return this.canLoadRewarded;
+            default:
+                return false;
+        }
     },
 
-    isAdLoaded(adType, callback) {
-        callNative('isAdLoaded', [adType], function (isLoaded) {
-            if (isFunction(callback)) {
-                callback(isLoaded);
-            }
-        });
+    isAdLoaded(adType) {
+        switch (adType) {
+            case _AdType.INTERSTITIAL:
+                return this.isInterstitialLoaded;
+            case _AdType.REWARDED:
+                return this.isRewardedLoaded;
+            default:
+                return false;
+        }
     }
 }
+
+
+window.addEventListener('onInterstitialLoaded', function (adInfo) {
+    YabbiAds.canLoadInterstitial = false;
+    YabbiAds.isInterstitialLoaded = true;
+});
+window.addEventListener('onInterstitialShown', function (adInfo) {
+    YabbiAds.canLoadInterstitial = true;
+    YabbiAds.isInterstitialLoaded = false;
+});
+
+window.addEventListener('onRewardedLoaded', function (adInfo) {
+    YabbiAds.canLoadRewarded = false;
+    YabbiAds.isRewardedLoaded = true;
+});
+window.addEventListener('onRewardedShown', function (adInfo) {
+    YabbiAds.canLoadRewarded = true;
+    YabbiAds.isRewardedLoaded = false;
+});
+
+
 
 if (typeof module !== undefined && module.exports) {
     module.exports = YabbiAds;
